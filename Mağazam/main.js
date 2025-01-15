56842f5ccf31fb8fc4f9b35debabc0d9e96bd343
Onur Akın <142675842+onurakinakbal@users.noreply.github.com>
2025-01-15 03:58:48 +0300

    Add files via upload

---
+++ Mağazam/main.js
@@ -0,0 +1,32 @@
+function myFuncWing() {
+    var x = document.getElementById("noNeos");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+function myFuncNeos() {
+    var y = document.getElementById("noWing");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  } 
+function myFunc() {
+  var z = document.getElementById("both");
+    var y = document.getElementById("noWing");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "block";
+    }
+    var x = document.getElementById("noNeos");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "block";
+  } 
+    
+  }
